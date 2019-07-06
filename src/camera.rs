4917b06dbad7a53ca17846e90a66b6fd53e0d6d6
Phil Ellison <phil.j.ellison@gmail.com>
2019-07-06 22:39:50 +0100

    Scattering, positionable camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 
+use std::f32::consts::PI;
 use crate::ray::*;
 use crate::vec3::*;
 
@@ -11,19 +12,32 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Camera {
+    pub fn new(
+        look_from: Vec3,
+        look_at: Vec3,
+        view_up: Vec3,
+        vfov: f32,
+        aspect: f32
+    ) -> Camera {
+        // Convert from degrees to radians
+        let theta = vfov * PI / 180.0;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+        let w = unit_vector(look_from - look_at);
+        let u = unit_vector(cross(view_up, w));
+        let v = cross(w, u);
         Camera {
-            origin: Vec3::new(0.0, 0.0, 0.0),
-            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
-            horizontal: Vec3::new(4.0, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0, 0.0)
+            origin: look_from,
+            lower_left_corner: look_from - half_width * u - half_height * v - w,
+            horizontal: 2.0 * half_width * u,
+            vertical: 2.0 * half_height * v
         }
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use rand::prelude::*;
+use std::f32::consts::PI;
 
 mod camera;
 use camera::*;
@@ -36,32 +37,47 @@ fn hit(world: &[Box<dyn Hittable>], ray: Ray, t_min: f32, t_max: f32) -> Option<
     best
 }
 
-/// Background linearly interpolates vertically from blue to white.
-fn colour(ray: Ray, world: &[Box<dyn Hittable>]) -> Vec3 {
-    if let Some(h) = hit(world, ray, 0.0, std::f32::MAX) {
-        return 0.5 * Vec3::new(
-            h.normal.x() + 1.0,
-            h.normal.y() + 1.0,
-            h.normal.z() + 1.0
-        );
+fn random_point_in_unit_sphere() -> Vec3 {
+    let mut rng = thread_rng();
+    loop {
+        let v = Vec3::new(rng.gen(), rng.gen(), rng.gen());
+        let p = 2.0 * v - Vec3::new(1.0, 1.0, 1.0);
+        if p.squared_length() <= 1.0 {
+            return p;
+        }
     }
+}
+
+fn colour(ray: Ray, world: &[Box<dyn Hittable>]) -> Vec3 {
+    if let Some(h) = hit(world, ray, 0.001, std::f32::MAX) {
+        let target = h.p + h.normal + random_point_in_unit_sphere();
+        return 0.5 * colour(Ray::new(h.p, target - h.p), world);
+    }
+
     let unit_direction = unit_vector(ray.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
+
+    // Background linearly interpolates vertically from blue to white.
     let white = Vec3::new(1.0, 1.0, 1.0);
     let blue = Vec3::new(0.5, 0.7, 1.0);
     (1.0 - t) * white + t * blue
 }
 
-fn render(world: &[Box<dyn Hittable>], camera: &Camera) -> Image {
-    let (width, height, num_samples) = (200, 100, 100);
+fn render(
+    width: usize,
+    height: usize,
+    num_samples: usize,
+    world: &[Box<dyn Hittable>],
+    camera: &Camera
+) -> Image {
     let mut image = Image::new(width, height);
-    let mut rng = rand::thread_rng();
+    let mut rng = thread_rng();
 
     for y in 0..height {
         for x in 0..width {
             let mut c = Vec3::new(0.0, 0.0, 0.0);
 
-            for s in 0..num_samples {
+            for _ in 0..num_samples {
                 let du: f32 = rng.gen();
                 let dv: f32 = rng.gen();
 
@@ -73,6 +89,10 @@ fn render(world: &[Box<dyn Hittable>], camera: &Camera) -> Image {
             }
 
             c /= num_samples as f32;
+
+            // rough gamma correction
+            c = Vec3::new(c.r().sqrt(), c.g().sqrt(), c.b().sqrt());
+
             let ir = (255.99 * c.r()) as u8;
             let ig = (255.99 * c.g()) as u8;
             let ib = (255.99 * c.b()) as u8;
@@ -84,14 +104,31 @@ fn render(world: &[Box<dyn Hittable>], camera: &Camera) -> Image {
 }
 
 fn world() -> Vec<Box<dyn Hittable>> {
-    let s1 = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
-    let s2 = Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0);
+    let r = PI / 4.0;
+    let s1 = Sphere::new(Vec3::new(-r, 0.0, -1.0), r);
+    let s2 = Sphere::new(Vec3::new( r, 0.0, -1.0), r);
     vec![Box::new(s1), Box::new(s2)]
 }
 
 fn main() {
+    let (width, height, num_samples) = (200, 100, 100);
     let world = world();
-    let camera = Camera::new();
-    let image = render(&world, &camera);
+    let look_from = Vec3::new(-2.0, 2.0, 1.0);
+    let look_at = Vec3::new(0.0, 0.0, -1.0);
+    let view_up = Vec3::new(0.0, 1.0, 0.0);
+    let camera = Camera::new(
+        look_from,
+        look_at,
+        view_up,
+        20.0,
+        width as f32 / height as f32
+    );
+    let image = render(
+        width,
+        height,
+        num_samples,
+        &world,
+        &camera
+    );
     image.save_to_ppm("hello_world.ppm").unwrap();
 }
